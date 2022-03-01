0894ff4767d4f7552430142c72c8813b8bfe6744
trevor-mill <trevor.million@chicagobooth.edu>
2022-03-01 15:13:20 +0000

    created actors

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,22 @@
 Rails.application.routes.draw do
 
+  # Routes for the Actor resource:
+
+  # CREATE
+  post("/insert_actor", { :controller => "actors", :action => "create" })
+          
+  # READ
+  get("/actors", { :controller => "actors", :action => "index" })
+  
+  get("/actors/:path_id", { :controller => "actors", :action => "show" })
+  
+  # UPDATE
+  
+  post("/modify_actor/:path_id", { :controller => "actors", :action => "update" })
+  
+  # DELETE
+  get("/delete_actor/:path_id", { :controller => "actors", :action => "destroy" })
+
+  #------------------------------
+
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,6 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 2022_03_01_151238) do
+
+  create_table "actors", force: :cascade do |t|
+    t.string "name"
+    t.date "dob"
+    t.text "bio"
+    t.string "image"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
 end
