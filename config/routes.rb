1ecf1d65140ba2eb054a7a5c88328ef4397331ba
trevor-mill <trevor.million@chicagobooth.edu>
2022-03-01 15:14:29 +0000

    tables all created

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,62 @@
 Rails.application.routes.draw do
 
+  # Routes for the Character resource:
+
+  # CREATE
+  post("/insert_character", { :controller => "characters", :action => "create" })
+          
+  # READ
+  get("/characters", { :controller => "characters", :action => "index" })
+  
+  get("/characters/:path_id", { :controller => "characters", :action => "show" })
+  
+  # UPDATE
+  
+  post("/modify_character/:path_id", { :controller => "characters", :action => "update" })
+  
+  # DELETE
+  get("/delete_character/:path_id", { :controller => "characters", :action => "destroy" })
+
+  #------------------------------
+
+  # Routes for the Movie resource:
+
+  # CREATE
+  post("/insert_movie", { :controller => "movies", :action => "create" })
+          
+  # READ
+  get("/movies", { :controller => "movies", :action => "index" })
+  
+  get("/movies/:path_id", { :controller => "movies", :action => "show" })
+  
+  # UPDATE
+  
+  post("/modify_movie/:path_id", { :controller => "movies", :action => "update" })
+  
+  # DELETE
+  get("/delete_movie/:path_id", { :controller => "movies", :action => "destroy" })
+
+  #------------------------------
+
+  # Routes for the Director resource:
+
+  # CREATE
+  post("/insert_director", { :controller => "directors", :action => "create" })
+          
+  # READ
+  get("/directors", { :controller => "directors", :action => "index" })
+  
+  get("/directors/:path_id", { :controller => "directors", :action => "show" })
+  
+  # UPDATE
+  
+  post("/modify_director/:path_id", { :controller => "directors", :action => "update" })
+  
+  # DELETE
+  get("/delete_director/:path_id", { :controller => "directors", :action => "destroy" })
+
+  #------------------------------
+
   # Routes for the Actor resource:
 
   # CREATE
